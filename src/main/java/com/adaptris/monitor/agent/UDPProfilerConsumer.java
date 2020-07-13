a3949e4ac05292f81a190fa87ff03c35b42c734f
Lewin Chan <lewin.chan@adaptris.com>
2020-07-13 12:18:03 +0100

    INTERLOK-3329 Add deprecated ConsumeDestination
    - lombok + lgtm ssupport
    - another consumer that doesn't use consume destination at all but needs
    one for backwards compat.

--- src/main/java/com/adaptris/monitor/agent/UDPProfilerConsumer.java
+++ src/main/java/com/adaptris/monitor/agent/UDPProfilerConsumer.java
@@ -1,38 +1,67 @@
 package com.adaptris.monitor.agent;
 
+import javax.validation.Valid;
+import com.adaptris.annotation.Removal;
 import com.adaptris.core.AdaptrisMessage;
 import com.adaptris.core.AdaptrisMessageFactory;
 import com.adaptris.core.AdaptrisPollingConsumer;
+import com.adaptris.core.ConsumeDestination;
 import com.adaptris.core.CoreException;
+import com.adaptris.core.util.DestinationHelper;
+import com.adaptris.core.util.LoggingHelper;
 import com.adaptris.monitor.agent.activity.ActivityMap;
 import com.adaptris.monitor.agent.json.EventJsonMarshaller;
+import lombok.Getter;
+import lombok.Setter;
 
 public class UDPProfilerConsumer extends AdaptrisPollingConsumer {
-    private int packetSize = 120400;
-    private EventJsonMarshaller jsonMarshaller;
+  private int packetSize = 120400;
+  private EventJsonMarshaller jsonMarshaller;
 
 
-    @Override
-    protected void prepareConsumer() throws CoreException {
-        jsonMarshaller = new EventJsonMarshaller();
-    }
+  /**
+   * The consume destination has no meaning.
+   *
+   */
+  @Deprecated
+  @Valid
+  @Removal(version = "4.0.0", message = "Has no meaning, and will be removed.")
+  @Getter
+  @Setter
+  private ConsumeDestination destination;
 
-    public int getPacketSize() {
-        return packetSize;
-    }
+  private transient boolean destinationWarningLogged = false;
 
-    public void setPacketSize(int packetSize) {
-        this.packetSize = packetSize;
-    }
+  @Override
+  protected void prepareConsumer() throws CoreException {
+    DestinationHelper.logConsumeDestinationWarning(destinationWarningLogged,
+        () -> destinationWarningLogged = true, getDestination(),
+        "{} uses destination, it's not required", LoggingHelper.friendlyName(this));
+    jsonMarshaller = new EventJsonMarshaller();
+  }
 
-    @Override
-    protected int processMessages() {
-        return 1;
-    }
+  public int getPacketSize() {
+    return packetSize;
+  }
 
-    public void processMessage(ActivityMap activityMap) {
-        String data = jsonMarshaller.marshallToJson(activityMap);
-        AdaptrisMessage msg = AdaptrisMessageFactory.defaultIfNull(getMessageFactory()).newMessage(data);
-        retrieveAdaptrisMessageListener().onAdaptrisMessage(msg);
-    }
+  public void setPacketSize(int packetSize) {
+    this.packetSize = packetSize;
+  }
+
+  @Override
+  protected int processMessages() {
+    return 1;
+  }
+
+  public void processMessage(ActivityMap activityMap) {
+    String data = jsonMarshaller.marshallToJson(activityMap);
+    AdaptrisMessage msg =
+        AdaptrisMessageFactory.defaultIfNull(getMessageFactory()).newMessage(data);
+    retrieveAdaptrisMessageListener().onAdaptrisMessage(msg);
+  }
+
+  @Override
+  protected String newThreadName() {
+    return DestinationHelper.threadName(retrieveAdaptrisMessageListener(), getDestination());
+  }
 }
