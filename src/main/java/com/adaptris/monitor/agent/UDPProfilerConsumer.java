05a4b3ff2aadd192b2e7003ec3f372a490c29baa
Aaron McGrath <aaron.mcgrath@adaptris.com>
2021-03-15 13:02:31 +0000

    INTERLOK-3566 Removed the destination object (not used anyway).

--- src/main/java/com/adaptris/monitor/agent/UDPProfilerConsumer.java
+++ src/main/java/com/adaptris/monitor/agent/UDPProfilerConsumer.java
@@ -1,42 +1,18 @@
 package com.adaptris.monitor.agent;
 
-import javax.validation.Valid;
-import com.adaptris.annotation.Removal;
 import com.adaptris.core.AdaptrisMessage;
 import com.adaptris.core.AdaptrisMessageFactory;
 import com.adaptris.core.AdaptrisPollingConsumer;
-import com.adaptris.core.ConsumeDestination;
 import com.adaptris.core.CoreException;
-import com.adaptris.core.util.DestinationHelper;
-import com.adaptris.core.util.LoggingHelper;
 import com.adaptris.monitor.agent.activity.ActivityMap;
 import com.adaptris.monitor.agent.json.EventJsonMarshaller;
-import lombok.Getter;
-import lombok.Setter;
 
 public class UDPProfilerConsumer extends AdaptrisPollingConsumer {
   private int packetSize = 120400;
   private EventJsonMarshaller jsonMarshaller;
 
-
-  /**
-   * The consume destination has no meaning.
-   *
-   */
-  @Deprecated
-  @Valid
-  @Removal(version = "4.0.0", message = "Has no meaning, and will be removed.")
-  @Getter
-  @Setter
-  private ConsumeDestination destination;
-
-  private transient boolean destinationWarningLogged = false;
-
   @Override
   protected void prepareConsumer() throws CoreException {
-    DestinationHelper.logConsumeDestinationWarning(destinationWarningLogged,
-        () -> destinationWarningLogged = true, getDestination(),
-        "{} uses destination, it's not required", LoggingHelper.friendlyName(this));
     jsonMarshaller = new EventJsonMarshaller();
   }
 
@@ -62,6 +38,6 @@ public class UDPProfilerConsumer extends AdaptrisPollingConsumer {
 
   @Override
   protected String newThreadName() {
-    return DestinationHelper.threadName(retrieveAdaptrisMessageListener(), getDestination());
+    return getClass().getSimpleName();
   }
 }
